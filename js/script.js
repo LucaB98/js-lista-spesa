ca20bd08eb373cc704388f1f416523ff8e330501
LucaB98 <lucabellan98@gmail.com>
2024-01-08 14:40:20 +0100

    added list

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 console.log('JS OK')
+
+const shoppingList = ['uova', 'latte', 'cereali', 'pasta', 'verdure', 'nutella' ];
+
+const list = document.getElementById('list')
+
+let items = 0;
+
+while(items < shoppingList.length){
+
+    let listItems = document.createElement('li');
+    listItems.textContent = shoppingList[items]
+    list.appendChild(listItems)
+
+    items++;
+}
