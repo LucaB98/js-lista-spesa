eee29a23785810d089f1a237bd64afbd35d59924
LucaB98 <lucabellan98@gmail.com>
2024-01-08 14:41:43 +0100

    added comments

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 console.log('JS OK')
-
+// creare un array list
 const shoppingList = ['uova', 'latte', 'cereali', 'pasta', 'verdure', 'nutella' ];
-
+// prendere gli elementi interessati
 const list = document.getElementById('list')
-
+// creare una variante dei elementi
 let items = 0;
-
+// mettere gli elemnti dentro la variante
 while(items < shoppingList.length){
 
     let listItems = document.createElement('li');
